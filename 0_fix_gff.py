7eeb2c9a53cdf7647bb9da272c80cdfde86ccead
MennoJansen <m.a.a.jansen2@students.uu.nl>
2023-02-01 15:24:10 +0100

    Improved and added documentation for 0_fix_gff.py

--- 0_fix_gff.py
+++ 0_fix_gff.py
@@ -1,14 +1,43 @@
-import sys
+"""Small script that removes duplicate lines from a file
+
+file : str
+    Path to file with duplicate lines
+
+"""
+import argparse
 import os
 from collections import OrderedDict
 
-file = "/drive/gff/Exoaq1.gff3"
-with open(file) as fin:
-    lines = (line.rstrip() for line in fin)
-    unique_lines = OrderedDict.fromkeys((line for line in lines if line))
+def remove_duplicate_lines(file : str):
+    """Removes duplicate lines from file and overwrites same file
 
-#print(unique_lines.keys())
-with open(file, 'w') as out:
-    for key in unique_lines.keys():
-        #print(key)
-        out.write(key + '\n')
+    file : str
+        Filepath of file with duplicate lines
+    
+    """
+    with open(file) as fin:
+        lines = (line.rstrip() for line in fin)
+        unique_lines = OrderedDict.fromkeys((line for line in lines if line))
+
+    with open(file, 'w') as out:
+        for key in unique_lines.keys():
+            out.write(key + '\n')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", dest= "file", help= "File with duplicate lines")
+
+    args = parser.parse_args()
+
+    cwd = os.getcwd()
+    if args.file is None:
+        print("file not specified")
+        exit()
+
+    file = cwd + "/" + args.file
+
+    if not file.endswith("/"):
+        file += "/"
+    print(f"Removing duplicate lines from: {file}")
+    remove_duplicate_lines(file)
+    print("Done! Exiting...")
